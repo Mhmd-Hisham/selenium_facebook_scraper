0ba9844c15993f7e4ce929fb2965e75bb331481f
Mohamed Hisham <Mohamed00Hisham@Gmail.com>
2018-08-15 20:45:42 +0200

    Update facebook_scraper_py3_version.py

--- facebook_scraper_py3_version.py
+++ facebook_scraper_py3_version.py
@@ -3,8 +3,9 @@
 #
 # TODO:-
 #     1- restrict connections to be https only.
-#     2- use another way to wait for element to load than delays.
-#     3- write full command-line interface for it?(if so, suggest options)
+#     2- Handle TimeoutExceptions for selenium.
+#     3- write full command-line interface for it.
+#     4- Fix XPATHs
 #
 #
 
@@ -18,8 +19,11 @@ import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def checkPageLoaded(driver):
+def check_page_loaded(driver):
     try:
         existing = False
         a = []
@@ -38,29 +42,24 @@ def sec_to_hms(sec):
     sec %= 60
     return '%02d:%02d:%02d' % (h, m, sec)
 
-def automate(driver, user, password):
+def automate(driver, user, password, timeout=30):
     """ Used to Automate user interaction and return html source page."""
+    wait = WebDriverWait(driver, timeout)
     driver.get("https://www.facebook.com")
 
-    assert "Facebook" in driver.title
-    time.sleep(10)
-
-    elem = driver.find_element_by_id("email")
+    elem = wait.until(EC.presence_of_element_located((By.ID, "email")))
     elem.send_keys(user)
-    elem = driver.find_element_by_id("pass")
+    elem =  wait.until(EC.presence_of_element_located((By.ID, "pass")))
     elem.send_keys(password)
     elem.send_keys(Keys.RETURN)
-    time.sleep(5)
-    driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/a").click()
-    time.sleep(5)
-    driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[3]/div/div[2]/div[2]/ul/li[3]/a").click()
-    time.sleep(2.5)
+    #wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "_1qv9"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/a"))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[3]/div/div[2]/div[2]/ul/li[3]/a"))).click()
 
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        if checkPageLoaded(driver):
+        if check_page_loaded(driver):
             break
-            continue
 
     return driver.page_source
 
@@ -89,14 +88,20 @@ def export_as_json(names, ids):
 
     data = dict()
     data["number of friends"] = len(ids)
-    data["friends"] = dict(zip(names,ids))
+    data["friends"] = dict(zip(ids, names))
 
     with open(filename, "w+", encoding='utf-8') as jsonfile:
         json.dump(data, jsonfile, indent=4)
 
     return filename
 
-def main():
+def import_from_htmlfile(path_to_htmlfile):
+    with open(path_to_htmlfile, "r", encoding="utf-8") as htmlfile:
+        htmlpage = htmlfile.read()
+
+    return htmlpage
+
+def get_login_data():
     user = input("Email: ")
     password = ""
 
@@ -107,6 +112,18 @@ def main():
             break
         print("Passwords didn't match!")
         password = ""
+
+    return user, password
+
+def get_login_data_from_file(filename):
+    with open(filename, "r", encoding="utf-8") as login_file:
+        user = login_file.readlines(1)
+        password = login_file.readlines(1)
+
+    return user, password
+
+def main():
+    user, password = get_login_data()
 
     options = Options()
     options.set_headless(headless=False)
@@ -119,12 +136,13 @@ def main():
     print("Loading html page... ", end="", flush=True)
     s = time.time()
     htmlpage = automate(driver, user, password)
+    # htmlpage = import_from_htmlfile("page.html")
     print("Done!.. (%s)\nProcessing data... \n"%sec_to_hms(time.time()-s))
     driver.quit()
 
     ids = re.findall('friend_list_item.+?data-profileid="(.+?)"', htmlpage)
     names = re.findall('friend_list_item.+?aria-label="(.+?)"', htmlpage)
-    
+
     print("%s friends found!"%len(ids))
 
     print("Exporting data as json file... ", end="", flush=True)
@@ -132,7 +150,7 @@ def main():
     print("Done!\nFile: "+filename)
 
     for i in range(len(ids)):
-        print(names[i], ":", ids[i])
+       print(names[i], ":", ids[i])
 
 if __name__ == "__main__":
     try:

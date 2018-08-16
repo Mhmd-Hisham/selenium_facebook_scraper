29e6bdb773482f6bc1ac13ca9a6b983b8044b361
AdhamGhoname <hghoname@yahoo.com>
2018-08-16 22:22:38 +0200

    Added BeautifulSoup for HTML extraction

--- facebook_scraper_py3_version.py
+++ facebook_scraper_py3_version.py
@@ -3,18 +3,14 @@
 #
 # TODO:-
 #     1- restrict connections to be https only.
-#     2- Handle TimeoutExceptions for selenium.
-#     3- write full command-line interface for it.
-#     4- Fix XPATHs
+#     2-
 #
 #
 
-import re
-import sys
-import csv
-import json
 import time
-import getpass
+import sys, os
+import json, csv, re
+import argparse, getpass
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -22,6 +18,21 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+
+def opt_parser():
+    """Parse command-line options."""
+    # verbose, headless, json, csv, html, htmlpage, loginfile
+    parser = argparse.ArgumentParser(description='Use Selenium & firefox to automate facebook login and scrape data.')
+    parser.add_argument("-v", "--verbose", help="Increase verbosity level.", action="store_true", default=False, dest="verbose")
+    parser.add_argument("--headless", help="Open firefox in headless mode", action="store_true", default=False, dest="headless")
+    parser.add_argument("--json", help="Export data in JSON format. (default)", default=True, action="store_true", dest="json")
+    parser.add_argument("--csv", help="Export data in CSV format.", default=False, action="store_true", dest="csv")
+    parser.add_argument("--html", help="Export data in HTML format.", default=False, action="store_true", dest="html")
+    parser.add_argument("--import-html", help="Import data from facebook htmlpage.", default=None, dest="htmlpage")
+    parser.add_argument("--login-data", help="Read login data from file.", default=None, dest="loginfile")
+
+    return parser.parse_args()
 
 def check_page_loaded(driver):
     try:
@@ -34,40 +45,12 @@ def check_page_loaded(driver):
         return existing
     except:
         return False
-
 def sec_to_hms(sec):
     h = sec / 3600
     sec %= 3600
     m = sec / 60
     sec %= 60
     return '%02d:%02d:%02d' % (h, m, sec)
-
-def automate(driver, user, password, timeout=30):
-    """ Used to Automate user interaction and return html source page."""
-    wait = WebDriverWait(driver, timeout)
-    driver.get("https://www.facebook.com")
-
-    elem = wait.until(EC.presence_of_element_located((By.ID, "email")))
-    elem.send_keys(user)
-    elem =  wait.until(EC.presence_of_element_located((By.ID, "pass")))
-    elem.send_keys(password)
-    elem.send_keys(Keys.RETURN)
-    #wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "_1qv9"))).click()
-    wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/a")))#.click()
-    #wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div[3]/div[1]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[3]/div/div[2]/div[2]/ul/li[3]/a"))).click()
-    driver.get("https://www.facebook.com/profile.php")
-    while (driver.current_url == "https://www.facebook.com/profile.php"):
-    	time.sleep(0.1)
-    
-    driver.get(driver.current_url[0:-2] + "/friends")
-
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        if check_page_loaded(driver):
-            break
-
-    return driver.page_source
-
 def export_as_csv(names, ids):
     filename = 'facebook_friends from %s.csv'%time.strftime("%Y-%m-%d %H-%M-%S")
 
@@ -76,10 +59,9 @@ def export_as_csv(names, ids):
         writer.writerow(['Name', 'Facebook profile id'])
 
         for i in range(len(ids)):
-            writer.writerow(name[i], ids[i])
+            writer.writerow([names[i], ids[i]])
 
     return filename
-
 def export_as_html(htmlpage):
     filename = 'facebook_friends from %s.html'%time.strftime("%Y-%m-%d %H-%M-%S")
 
@@ -87,7 +69,6 @@ def export_as_html(htmlpage):
         htmlfile.write(htmlpage)
 
     return filename
-
 def export_as_json(names, ids):
     filename = 'facebook_friends from %s.json'%time.strftime("%Y-%m-%d %H-%M-%S")
 
@@ -99,13 +80,11 @@ def export_as_json(names, ids):
         json.dump(data, jsonfile, indent=4)
 
     return filename
-
 def import_from_htmlfile(path_to_htmlfile):
     with open(path_to_htmlfile, "r", encoding="utf-8") as htmlfile:
         htmlpage = htmlfile.read()
 
     return htmlpage
-
 def get_login_data():
     user = input("Email: ")
     password = ""
@@ -119,7 +98,6 @@ def get_login_data():
         password = ""
 
     return user, password
-
 def get_login_data_from_file(filename):
     with open(filename, "r", encoding="utf-8") as login_file:
         user = login_file.readlines(1)
@@ -127,34 +105,116 @@ def get_login_data_from_file(filename):
 
     return user, password
 
-def main():
-    user, password = get_login_data()
+def automate(driver, user, password, timeout=30, verbose=False):
+    """ Used to Automate user interaction and return html source page."""
+    wait = WebDriverWait(driver, timeout)
+    driver.get("https://www.facebook.com")
 
-    options = Options()
-    options.set_headless(headless=False)
+    elem = wait.until(EC.presence_of_element_located((By.ID, "email")))
+    elem.send_keys(user)
+    elem =  wait.until(EC.presence_of_element_located((By.ID, "pass")))
+    elem.send_keys(password)
+    elem.send_keys(Keys.RETURN)
+    wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/div/div/div/div[2]/div[1]/div[1]/div/a")))#.click()
+    driver.get("https://www.facebook.com/profile.php")
+    while (driver.current_url == "https://www.facebook.com/profile.php"):
+    	time.sleep(0.2)
+
+    driver.get((driver.current_url[0:-2] if "#_" == driver.current_url[-2:] else driver.current_url) + "/friends")
+
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        if check_page_loaded(driver):
+            break
+
+    return driver.page_source
+
+def start(options):
+    """Start the program and handle options"""
+    if options.verbose:
+        s = time.time()
+
+    if options.loginfile:
+        try:
+            user, password = get_login_data_from_file(options.loginfile)
+        except Exception as Error:
+            print(Error)
+            if 'y' in input("Do you want to get login data from stdout?(y/n) ").lower():
+                user, password = get_login_data()
+            else:
+                sys.exit(0)
+    else:
+        user, password = get_login_data()
+
+    print("Loading html page... ", end="", flush=True)
+    firefox_options = Options()
+    firefox_options.set_headless(headless=options.headless)
 
     ffprofile = webdriver.FirefoxProfile()
     ffprofile.set_preference("dom.webnotifications.enabled", False)
+    driver = webdriver.Firefox(firefox_profile=ffprofile, firefox_options=firefox_options)
 
-    driver = webdriver.Firefox(firefox_profile=ffprofile, firefox_options=options)
+    htmlpage = automate(driver, user, password, verbose=options.verbose)
 
-    print("Loading html page... ", end="", flush=True)
-    s = time.time()
-    htmlpage = automate(driver, user, password)
-    # htmlpage = import_from_htmlfile("page.html")
-    print("Done!.. (%s)\nProcessing data... \n"%sec_to_hms(time.time()-s))
     driver.quit()
 
-    friend_list_items = re.findall("friend_list_item(.+?)friends_tab", htmlpage)
-    friend_list_items = "".join(friend_list_items)
-    ids = re.findall("www.facebook.com\/(.+?)[&]?[a]?[m]?[p]?[;]?[\?]?fref=pb&amp;hc_location=", friend_list_items)
-    names = re.findall('friend_list_item.+?aria-label="(.+?)"', htmlpage)
+    if options.verbose:
+        print("Done!.. (%s)\n"%sec_to_hms(time.time()-s))
 
-    print("%s friends found!"%len(ids))
+    return htmlpage
 
-    print("Exporting data as json file... ", end="", flush=True)
-    filename = export_as_json(names, ids)
-    print("Done!\nFile: "+filename)
+def main():
+    # verbose, headless, json, csv, html, htmlpage, loginfile
+    options = opt_parser()
+
+    if options.htmlpage:
+        try:
+            htmlpage = import_from_htmlfile(options.htmlpage)
+        except Exception as error:
+            print(error)
+            if y in input("Do you want to scrape data online?(y/n) ").lower():
+                htmlpage = start(options)
+            else:
+                return 0
+    else:
+        htmlpage = start(options)
+
+    if options.verbose:
+        print("Processing data... ")
+
+    friend_list_items = BeautifulSoup(htmlpage, "html.parser").find_all("li", class_="_698")
+    names = []
+    ids = []
+    for item in friend_list_items:
+    	try:
+    		if (item.find_all("a")[2].text[0].isdigit()):
+    			continue
+    		names.append(item.find_all("a")[2].text)
+    		ids.append(re.findall("www.facebook.com\/(.+?)[&]?[a]?[m]?[p]?[;]?[\?]?fref=pb", item.find_all("a")[2].attrs['href'])[0])
+    	except:
+    		names.append(item.find_all("a")[1].text + "(Deactivated)")
+    		try:
+    			ids.append(item.find_all("a")[0].attrs['data-profileid'])
+    		except:
+    			continue
+    #print("names length: " + len(names) + " ids length: " + len(ids))   			
+    if options.verbose:
+        print("%s friends found!"%len(ids))
+
+    if options.json:
+        print("Exporting data as json file... ", end="", flush=True)
+        filename = export_as_json(names, ids)
+        print("Done. File: '%s'"%filename)
+
+    if options.csv:
+        print("Exporting data as csv file... ", end="", flush=True)
+        filename = export_as_csv(names, ids)
+        print("Done. File: '%s'"%filename)
+
+    if options.html:
+        print("Exporting data as html file... ", end="", flush=True)
+        filename = export_as_html(htmlpage)
+        print("Done. File: '%s'"%filename)
 
     for i in range(len(ids)):
        print(names[i], ":", ids[i])
@@ -162,7 +222,8 @@ def main():
 if __name__ == "__main__":
     try:
         main()
+
     except KeyboardInterrupt:
-        print("Keyboard Interruption! exitting...")
+        print("Keyboard Interruption! exitting... ")
 
     sys.exit(0)
